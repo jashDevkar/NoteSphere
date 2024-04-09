ba0ab2423b97e6f02c636c6b5e77b7f540496291
jashDevkar <devkarjash1054@gmail.com>
2024-04-09 21:10:52 +0530

    added loading

--- Main/Main.jsx
+++ Main/Main.jsx
@@ -4,15 +4,27 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import Navigation from '../src/Navigation/Navigation'
 import auth from '@react-native-firebase/auth';
 import Signin from '../Google/Signin'
+import Activity from '../DataBaseFetch/FetchingActivity';
 
 
 const Stack = createNativeStackNavigator()
 const Main = () => {
     const [user, setUser] = useState();
+    const [loading, setLoading] = useState(true)
     useEffect(() => {
-        const subscriber = auth().onAuthStateChanged(user=> setUser(user));
+        const subscriber = auth().onAuthStateChanged(user => {
+            setUser(user);
+            setLoading(false);
+        });
         return subscriber;
     }, []);
+
+
+    if(loading){
+        return(
+            <Activity/>
+        )
+    }
 
     return (
         <NavigationContainer>
