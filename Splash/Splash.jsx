72fc249223c18406f1d44eae16ab9546c43edf29
jashDevkar <devkarjash1054@gmail.com>
2024-05-17 18:03:21 +0530

    kn kiya hu

--- Splash/Splash.jsx
+++ Splash/Splash.jsx
@@ -5,7 +5,7 @@ const Splash = () => {
   return (
     <View style={{flex:1,justifyContent:'center',alignItems:'center',gap:5}}>
       <Text style={{fontSize:36,color:'black',fontWeight:'bold'}}>Notes-Sphere</Text>
-      <Text style={{color:'black',fontSize:16}}>Your altimate notes provider</Text>
+      <Text style={{color:'black',fontSize:16}}>Your ultimate notes provider</Text>
       <View style={{display:'flex', flexDirection:'row',justifyContent:'center',alignItems:'center',gap:10,marginTop:10}}>
         <Text style={{color:'black'}}>Fetching...</Text>
       <ActivityIndicator color={'black'}/>
